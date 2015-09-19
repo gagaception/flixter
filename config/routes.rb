de4a8cd0eb1b93a5a0e28cbaa84b9a5ef7174df2
Aziz <sharipov.aziz.sh@gmail.com>
2015-09-19 07:56:47 +0000

    Added home page controller and view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Flixter::Application.routes.draw do
+  root 'static_page#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,5 @@ Flixter::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end

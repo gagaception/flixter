bc6d95387b54f95e30f49e6c2dbcb2380c2edb2a
Aziz <sharipov.aziz.sh@gmail.com>
2015-10-04 05:25:15 +0000

    Styling instructor's page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Flixter::Application.routes.draw do
   devise_for :users
+  resource     :dashboard, :only => [:show]
   root 'static_page#index'
   resources     :courses, :only => [:index, :show] do
     resources   :enrollments, :only => [:create]
@@ -12,7 +13,7 @@ Flixter::Application.routes.draw do
       resources :lessons,  :only => [:new, :create]
     end
     resources   :courses,  :only => [:new, :create, :show] do
-      resources :sections, :only => [:new, :create] 
+      resources :sections, :only => [:create] 
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
